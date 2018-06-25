3bd091d179b4a1db675023f3b88a739a64499f7a
xuyz <756288646@qq.com>
2018-06-25 14:02:03 +0800

    1

--- config/filesystems.php
+++ config/filesystems.php
@@ -50,7 +50,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
@@ -65,7 +65,7 @@ return [
 
         'admin' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => public_path('storage'),
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
